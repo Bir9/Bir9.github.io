eb301edf48f2a33378d66b300943ad98dcee303b
Adib Shargh <76136398+Bir9@users.noreply.github.com>
2022-01-27 10:51:08 -0500

    Update Date.js

--- JS/Date.js
+++ JS/Date.js
@@ -2,13 +2,13 @@ let ageEl = document.getElementById("age");
 
 window.onload = function() {
     function onLoad() {
-        getAge()
+        getAge();
     }
 }
 
 function getAge() {
     var age = Math.floor((Date.now() - new Date("2008-01-19")) / 31536000000);
 
-    ageEl.innerText = age.toString()
-    console.log(typeof()age)
+    ageEl.innerText = age.toString();
+    console.log(typeof()age, age);
 }
